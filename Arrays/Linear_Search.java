f108c32d7fdf05b8d4a7c570be2bf2f51547e6d8
chai-aur-react <shambharkaranuj@gmail.com>
2026-03-02 01:40:58 +0530

    added linear search problem in arrays

---
+++ Arrays/Linear_Search.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Linear_Search {
+    public static int linearSearch(int n, int num, int []arr){
+        for(int i=0;i<n;i++){
+            if(arr[i]==num)return i;
+        }
+        return -1;
+    }
+}
