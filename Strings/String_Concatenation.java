1bf063f6513a9bdb12776bb80e67af1441e4ce58
chai-aur-react <shambharkaranuj@gmail.com>
2026-03-02 17:58:01 +0530

    add string concatenation

---
+++ Strings/String_Concatenation.java
@@ -0,0 +1,29 @@
+import java.util.* ;
+import java.io.*; 
+
+public class String_Concatenation {
+
+	public static String stringConcatenation(String num1, String num2) {
+		// Write your code here.
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+
+        int carry=0;
+
+        StringBuilder sb=new StringBuilder();
+        while(i>=0 || j>=0 || carry!=0){
+            int digit1=(i>=0)?num1.charAt(i)-'0':0;
+            int digit2=(j>=0)?num2.charAt(j)-'0':0;
+
+            int sum=digit1+digit2+carry;
+            sb.append(sum%10);
+            carry=sum/10;
+
+            i--;
+            j--;
+
+        }
+        return sb.reverse().toString();
+	}
+
+}
